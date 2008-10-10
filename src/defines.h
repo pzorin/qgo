4a51fa50978c5b3cdd725a95b2821238feb02456
peterius <peterius@cb95479e-180f-4b2e-960d-988ef6f66277>
2008-10-10 04:32:12 +0000

    added preferences and mainwindow extern, changed marks to be cleaner and added some, added gamedialog motive defines and moved globals.h into this to have just the one

--- src/defines.h
+++ src/defines.h
@@ -5,6 +5,8 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
+#include "audio.h"
+
 #include <QtCore>
 
 /*
@@ -41,17 +43,36 @@
 /*
 * Marks used in editing a game
 */
-#define MARK_TERRITORY_VISITED     99
-#define MARK_TERRITORY_DAME       999
-#define MARK_SEKI                  10
-#define MARK_TERRITORY_DONE_BLACK 997
-#define MARK_TERRITORY_DONE_WHITE 998
+/* FIXME, I multiplied these by 10 because they prevented the use of countLiberties
+ * by checkFalseEye by corrupting the stone.  It seems to me rather silly to
+ * have defines like these in addition to the marking system, but they are sort
+ * of internal marks.  At any rate, things still seem to work so I guess
+ * this fixed things.  But its still ugly and there could still be more problems */
+#define MARK_TERRITORY_VISITED    0x0800
+#define MARK_TERRITORY_DAME       0x0400
+#define MARK_SEKI                 0x1000
+#define MARK_TERRITORY_DONE_BLACK 0x0200
+#define MARK_TERRITORY_DONE_WHITE 0x0100
+
+//first nibble is for stone color/erase
+#define MX_STONEDEAD	0x8000
+#define MX_STONEEDIT	0x4000
+
 
 /*
 * GNUgo default level
 */
 #define GNUGO_LEVEL 10
 
+/* Game refusal motives... like we need a reason.  */
+#define	GD_REFUSE_NOTOPEN		0
+#define GD_REFUSE_DECLINE		1
+#define GD_REFUSE_CANCEL		2
+#define GD_REFUSE_INGAME		3
+#define GD_REFUSE_NODIRECT		4
+#define GD_INVALID_PARAMETERS		5
+#define GD_OPP_NO_NMATCH		6
+#define GD_RESET			7
 
 /*
 * Enum definitions
@@ -59,14 +80,14 @@
 enum StoneColor { stoneNone, stoneWhite, stoneBlack, stoneErase };
 enum GameMode { modeNormal, modeObserve, modeMatch, modeTeach, modeComputer, modeReview };
 enum GamePhase { phaseInit, phaseOngoing, phaseEdit, phaseNavTo, phaseScore, phaseEnded};
-enum MarkType { markNone, markSquare, markCircle, markTriangle, markCross, markText, markNumber, markTerrBlack, markTerrWhite };
+enum MarkType { markNone, markSquare, markCircle, markTriangle, markCross, markText, markNumber, markTerrBlack, markTerrWhite, markKoMarker };
 enum Codec { codecNone, codecBig5, codecEucJP, codecJIS, codecSJIS, codecEucKr, codecGBK, codecTscii };
 enum player_type {HUMAN=0,COMPUTER} ;
 enum assessType { noREQ, FREE, RATED, TEACHING };
 
 enum State { stateVarBegin, stateNode, stateVarEnd };
 enum Property { moveBlack, moveWhite, editBlack, editWhite, editErase, comment, editMark, unknownProp, nodeName, timeLeft, openMoves, nextMove};
-enum TimeSystem { none, absolute, byoyomi, canadian };
+enum TimeSystem { none, absolute, byoyomi, canadian, tvasia };
 
 enum CursorType { cursorIdle, cursorGhostBlack , cursorGhostWhite , cursorWait , cursorNavTo };
 
@@ -80,7 +101,11 @@ enum InfoType {PLAYER, GAME, MESSAGE, YOUHAVEMSG, SERVERNAME,
 		WHO, STATS, GAMES, NONE, HELP, CHANNELS, SHOUT,
 		PLAYER27, PLAYER27_START, PLAYER27_END, GAME7, GAME7_START,
 		PLAYER42, PLAYER42_START, PLAYER42_END, WS};
-enum GSName {IGS, NNGS, LGS, WING, CTN, CWS, DEFAULT, GS_UNKNOWN};
+/* ConnectionType in line with ui comboBox_server entry. FIXME to make more securely connected */
+enum ConnectionType { TypeNone = 0, TypeIGS, TypeWING, TypeLGS, TypeORO, TypeUNKNOWN,
+			TypeNNGS, TypeCTN, TypeCWS, TypeDEFAULT };
+/* TypeCyberORO?? just IGS, and CyberORO, instead of with Type?? FIXME 
+ * and reconcile with mainwindow_settings.h */
 
 /*
  * Go Text Protocol (GnuGo) enums
@@ -131,7 +156,7 @@ struct GameInfo
 };
 
 
-struct Game
+/*struct Game
 {
 	QString nr;
 	QString	wname;
@@ -179,7 +204,81 @@ struct Player
 		nmatch_stonesMin, nmatch_stonesMax,
 		nmatch_KoryoMin, nmatch_KoryoMax;
 
+};*/
+
+/* This should really probably have access functions or
+ * something and then friend the mainwindow settings code so
+ * that only it can change it */
+struct _preferences
+{
+	bool nmatch_black;
+	bool nmatch_white;
+	bool nmatch_nigiri;
+	QString nmatch_handicap;
+	bool draw_ko_marker;
+	bool observe_outside_on_doubleclick;
+	int default_size;
+	int default_komi;
+	
+	int default_stonesmaintime;
+	int default_stonestime;
+	int default_stones;
+	int default_byomaintime;
+	int default_byoperiodtime;
+	int default_byoperiods;
+	int default_asiamaintime;
+	int default_asiaperiodtime;
+	int default_asiaperiods;
+	void fill(void)
+	{
+		QSettings settings;
+		
+		nmatch_black = settings.value("NMATCH_BLACK").toBool();
+		nmatch_white = settings.value("NMATCH_WHITE").toBool();
+		nmatch_nigiri = settings.value("NMATCH_NIGIRI").toBool();
+		nmatch_handicap = settings.value("NMATCH_HANDICAP").toString();
+		
+		draw_ko_marker = (settings.value("KOMARKER") == 1);
+		observe_outside_on_doubleclick = (settings.value("OBSERVEOUTSIDE") == 1);
+		
+		default_size = settings.value("DEFAULT_SIZE").toInt();
+		default_komi = settings.value("DEFAULT_KOMI").toInt();
+		
+		default_stonesmaintime = settings.value("DEFAULT_STONESMAIN").toInt();
+		default_stonestime = settings.value("DEFAULT_STONEST").toInt();
+		default_stones = settings.value("DEFAULT_STONES").toInt();
+		default_byomaintime = settings.value("DEFAULT_BYOMAIN").toInt();
+		default_byoperiodtime = settings.value("DEFAULT_BYOPERIODT").toInt();
+		default_byoperiods = settings.value("DEFAULT_BYOPERIODS").toInt();
+		default_asiamaintime = settings.value("DEFAULT_TVASIAMAIN").toInt();
+		default_asiaperiodtime = settings.value("DEFAULT_TVASIAPERIODT").toInt();
+		default_asiaperiods = settings.value("DEFAULT_TVASIAPERIODS").toInt();
+		
+		
+	}
+	void save(void)
+	{
+		QSettings settings;
+		
+		settings.setValue("DEFAULT_STONESMAIN", default_stonesmaintime);
+		settings.setValue("DEFAULT_STONEST", default_stonestime);
+		settings.setValue("DEFAULT_STONES", default_stones);
+		settings.setValue("DEFAULT_BYOMAIN", default_byomaintime);
+		settings.setValue("DEFAULT_BYOPERIODT", default_byoperiodtime);
+		settings.setValue("DEFAULT_BYOPERIODS", default_byoperiods);
+		settings.setValue("DEFAULT_TVASIAMAIN", default_asiamaintime);
+		settings.setValue("DEFAULT_TVASIAPERIODT", default_asiaperiodtime);
+		settings.setValue("DEFAULT_TVASIAPERIODS", default_asiaperiods);
+	}
 };
 
+/* There's only one of each of these, and they're both
+ * used in weird places, so we'll make them global */
+extern _preferences preferences;
+extern class MainWindow * mainwindow;
+
+#ifdef Q_WS_WIN
+extern QString applicationPath;
+#endif
 
 #endif
