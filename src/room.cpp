0ae97ed0884c22c915b93e93775c1f28d468ea78
peterius <peterius@cb95479e-180f-4b2e-960d-988ef6f66277>
2009-02-25 03:24:23 +0000

    integrated room dispatch and added semi working code to store rank filter as preferences

--- src/room.cpp
+++ src/room.cpp
@@ -1,11 +1,12 @@
 #include "room.h"
 #include "listviews.h"
-#include "network/roomdispatch.h"
-#include "network/talkdispatch.h"
+#include "roomregistries.h"
+#include "talk.h"
 #include "network/messages.h"
-#include "network/gamedialogdispatch.h"
+#include "gamedialog.h"
 #include "mainwindow.h"
 #include "network/playergamelistings.h"
+#include "boarddispatch.h"	//so we can remove observers
 
 Room::Room()
 {
@@ -26,7 +27,9 @@ Room::Room()
 	/* There are further strangenesses here because of the account_name
 	* for making our player listing blue */
 	setupUI();
-	dispatch = new RoomDispatch(this);
+	
+	playerListingRegistry = 0;
+	playerListingIDRegistry = 0;
 	
 	players = 0;
 	games = 0;
@@ -77,9 +80,18 @@ void Room::setupUI(void)
 	playerSortProxy = new PlayerSortProxy();
 	playerListModel = new PlayerListModel();
 	playerSortProxy->setSourceModel(playerListModel);
-	//playerSortProxy->setDynamicSortFilter(true);
+	/* FIXME the below seems to have no affect, so I commented it out.
+	 * I changed references to the listmodel to the sortproxy in the id
+	 * registries, in the hope that they would... well I think that was the
+	 * wrong way, but I can't be sure since it could be an issue with
+	 * the network connection as well... anyway, if I get it working
+	 * correctly, probably more like the way it was, then I should change
+	 * the sortproxy references back maybe... */
 	
+   // playerView->setIconSize(QSize(20, 20));
 	playerView->setModel(playerSortProxy);
+	playerSortProxy->setDynamicSortFilter(true);
+	//connect(playerListModel, SIGNAL(dataChanged(QModelIndex())), playerSortProxy, SLOT(clear()));
 	playerView->header()->setSortIndicatorShown ( true );
 	playerView->setColumnWidth ( 0, 40 );
 	playerView->setColumnWidth ( 1, 100 );
@@ -108,6 +120,8 @@ void Room::setupUI(void)
 	ui.ListView_players->setColumnWidth ( 10, 50 );
 //	ui.ListView_players->setColumnWidth ( 11, 25 );*/
 
+	whoBox1->setCurrentIndex(0);
+	whoBox2->setCurrentIndex(0);
 	/* Maybe also a "clicked" for something? */
 	connect(playerView, SIGNAL(doubleClicked(const QModelIndex &)), SLOT(slot_playersDoubleClicked(const QModelIndex &)));
 	connect(playerView, SIGNAL(customContextMenuRequested (const QPoint &)), SLOT(slot_showPopup(const QPoint &)));
@@ -122,7 +136,9 @@ void Room::setupUI(void)
 
 Room::~Room()
 {
-	delete dispatch;
+	delete playerListingRegistry;
+	delete playerListingIDRegistry;
+	delete gameListingRegistry;
 	
 	/* FIXME: Should probably be part of something else this P G stuff */
 	mainwindow->statusUsers->setText(" P: 0");
@@ -138,6 +154,15 @@ Room::~Room()
 	delete playerListModel;
 	delete playerSortProxy;
 	delete gamesListModel;
+	disconnect(whoBox1, SIGNAL(currentIndexChanged(int)), 0, 0);
+	disconnect(whoBox2, SIGNAL(currentIndexChanged(int)), 0, 0);
+	
+	
+	QSettings settings;
+	settings.setValue("LOWRANKFILTER", whoBox1->currentIndex());
+	settings.setValue("HIGHRANKFILTER", whoBox2->currentIndex());
+	whoBox1->setCurrentIndex(0);
+	whoBox2->setCurrentIndex(0);
 }
 
 void Room::onError(void)
@@ -147,11 +172,24 @@ void Room::onError(void)
 	//mainwindow->onConnectionError();
 }
 
-void Room::onConnectionAssignment(QString username)
+void Room::setConnection(NetworkConnection * c)
 {
-	playerListModel->setAccountName(username);	
+	connection = c;
+	if(connection->playerTrackingByID())
+	{
+		qDebug("Creating player ID Registry");
+		playerListingIDRegistry = new PlayerListingIDRegistry(playerListModel);
+	}
+	else
+	{
+		qDebug("Creating player Registry");
+		playerListingRegistry = new PlayerListingRegistry(playerListModel);
+	}
+	gameListingRegistry = new GameListingRegistry(gamesListModel);
+	
+	playerListModel->setAccountName(connection->getUsername());
 	// FIXME, what about observerListModels when they come up on boards?
-	unsigned long flags = dispatch->getPlayerListColumns();
+	unsigned long flags = connection->getPlayerListColumns();
 	if(flags & PL_NOWINSLOSSES)
 	{
 		playerView->hideColumn(5);
@@ -161,10 +199,20 @@ void Room::onConnectionAssignment(QString username)
 	{
 		playerView->hideColumn(9);	
 	}
+	
+	QSettings settings;
+	QVariant var = settings.value("LOWRANKFILTER");
+	if(var != QVariant())
+	{
+		whoBox1->setCurrentIndex(var.toInt());
+		whoBox2->setCurrentIndex(settings.value("HIGHRANKFILTER").toInt());
+	}
 }
 
 void Room::updateRoomStats(void)
 {
+	/* With sort, it might be better to get these from the registries? 
+	 * possible FIXME */
 	players = playerListModel->rowCount(QModelIndex());
 	games = gamesListModel->rowCount(QModelIndex());
 	
@@ -177,8 +225,8 @@ void Room::updateRoomStats(void)
 void Room::slot_playersDoubleClicked(const QModelIndex & index)
 {
 	QModelIndex translated = playerSortProxy->mapToSource(index);
-	const PlayerListing * opponent = playerListModel->playerListingFromIndex(translated);
-	dispatch->sendStatsRequest(*opponent);
+	PlayerListing * opponent = playerListModel->playerListingFromIndex(translated);
+	sendStatsRequest(*opponent);
 }
 
 void Room::slot_showPopup(const QPoint & iPoint)
@@ -188,16 +236,16 @@ void Room::slot_showPopup(const QPoint & iPoint)
 	{
     		QMenu menu(playerView);
     		menu.addAction(tr("Match"), this, SLOT(slot_popupMatch()));
-		menu.addAction(tr("Talk"), this, SLOT(slot_popupTalk()));
+			menu.addAction(tr("Talk"), this, SLOT(slot_popupTalk()));
     		menu.exec(playerView->mapToGlobal(iPoint));
 	}
 }
 
 void Room::slot_showGamesPopup(const QPoint & iPoint)
 {
-	if(!dispatch)
+	if(!connection)
 		return;
-	if(!dispatch->supportsObserveOutside())
+	if(!connection->supportsObserveOutside())
 		return;
 	popup_item = gamesView->indexAt(iPoint);
 	if (popup_item != QModelIndex())
@@ -227,7 +275,7 @@ void Room::slot_popupMatch(void)
 	QModelIndex translated = playerSortProxy->mapToSource(popup_item);
 	const PlayerListing * listing = playerListModel->playerListingFromIndex(translated);
 	
-	dispatch->sendMatchInvite(*listing);
+	connection->sendMatchInvite(*listing);
 }
 
 void Room::slot_popupTalk(void)
@@ -238,13 +286,13 @@ void Room::slot_popupTalk(void)
 void Room::slot_popupObserveOutside(void)
 {
 	const GameListing * g = gamesListModel->gameListingFromIndex(popup_item);
-	dispatch->sendObserveOutside(*g);
+	connection->sendObserveOutside(*g);
 }
 
 void Room::slot_popupJoinObserve(void)
 {
 	const GameListing * g = gamesListModel->gameListingFromIndex(popup_item);
-	dispatch->sendObserve(*g);
+	connection->sendObserve(*g);
 }
 
 //observe
@@ -252,16 +300,16 @@ void Room::slot_gamesDoubleClicked(const QModelIndex & index)
 {
 	const GameListing * g = gamesListModel->gameListingFromIndex(index);
 	if(preferences.observe_outside_on_doubleclick &&
-		  dispatch->supportsObserveOutside() && !g->isRoomOnly)
-		dispatch->sendObserveOutside(*g);
+		  connection->supportsObserveOutside() && !g->isRoomOnly)
+		connection->sendObserveOutside(*g);
 	else
-		dispatch->sendObserve(*g);
+		connection->sendObserve(*g);
 }
 
 void Room::slot_refreshPlayers(void)
 {
 	clearPlayerList();
-	dispatch->sendPlayersRequest();
+	connection->sendPlayersRequest();
 }
 
 void Room::clearPlayerList(void)
@@ -278,12 +326,12 @@ void Room::slot_refreshGames(void)
 {
 	/* First clear list.  There doesn't
 	* seem to be a good way to update it
-	* or update everything.  Since we're
+	* or update everything [IGS issue].  Since we're
 	* getting all info again anyway... 
 	* same with players and perhaps 
 	* observers as well. */
 	clearGamesList();
-	dispatch->sendGamesRequest();
+	connection->sendGamesRequest();
 }
 
 /* This code was taken from mainwindow_server.cpp
@@ -345,13 +393,185 @@ void Room::slot_setRankSpreadView(void)
 		rkMax = whoBox2->currentText();
 	} 
 
-	playerSortProxy->setFilter(dispatch->rankToScore(rkMin), dispatch->rankToScore(rkMax));
+	playerSortProxy->setFilter(connection->rankToScore(rkMin), connection->rankToScore(rkMax));
 	qDebug( "rank spread : %s - %s" , rkMin.toLatin1().constData() , rkMax.toLatin1().constData());
 }
 
 void Room::slot_showOpen(int state)
 {
 	playerSortProxy->setFilter(state);
+}
+
+void Room::talkOpened(Talk * d)
+{
+	mainwindow->talkOpened(d);
+}
+
+void Room::recvToggle(int type, bool val)
+{
+	mainwindow->slot_checkbox(type, val);
+}
+
+GameListing * Room::registerGameListing(GameListing * l)
+{
+	return gameListingRegistry->getEntry(l->number, l);
+}
+
+/* This appears to be unused, recvGameListing does stuff
+ * with "->running" FIXME */
+void Room::unRegisterGameListing(unsigned int key)
+{
+	gameListingRegistry->deleteEntry(key);
+}
+
+PlayerListing * Room::getPlayerListing(const QString & name)
+{
+	PlayerListing * p = playerListingRegistry->getEntry(name);
+#ifdef FIXME
+	/* this is tricky FIXME */
+	if(!p)
+		connection->sendStatsRequest(name);		
+#endif //FIXME
+	/* Should probably return & FIXME */
+	return p;
+}
+
+PlayerListing * Room::getPlayerListing(const unsigned int id)
+{
+	PlayerListing * p = playerListingIDRegistry->getEntry(id);
+	// FIXME
+	/*if(!p)
+		connection->sendStatsRequest(id);*/		
+	return p;
+}
+
+/* Remember that this and getPlayerListing do return 0.
+ * getEntry functions like getIfEntry because getNewEntry
+ * is not defined, since we create registry entries for
+ * the games/player registries by passing them */
+GameListing * Room::getGameListing(unsigned int key)
+{
+	GameListing * l = gameListingRegistry->getEntry(key);
+	/* I'm not sure we'd ever want to automatically
+	 * requestGameStats.  Either the listing is on the way
+	 * or we request it from some other place.  The main
+	 * thing is we don't want to be requesting it from
+	 * the games listing messages every time we try to
+	 * get an existing listing.  But if its a problem
+	 * to just comment out the below two lines, then
+	 * we'll need a getIfGameListing as with the 
+	 * getIfBoardDispatch FIXME */
+	//if(!l)
+	//	connection->requestGameStats(key);
+	return l;
+}
+
+/* Called from getNewEntry in BoardDispatchRegistry in networkconnection.cpp */
+class BoardDispatch * Room::getNewBoardDispatch(unsigned int key)
+{
+	/* Oro might pass a game code rather than a number here.  But
+	* the listings sometimes only have numbers.  We need to
+	* look up in game stuff like moves and results by game code.
+	* we don't have the number there.  But this means always
+	* passing game codes to this, but then we can't use getGameListing
+	* initially.  Even if we use the number on the initial board get/create
+	* and the game code thereafter, assuming there aren't collisions in
+	* the id spaces, we still have to alter this to be aware of the
+	* ORO protocol.  Its a fucking mess. I think we better just store
+	* everything by the key and have a separate lookup, code to key */
+	GameListing * listing = getGameListing(key);
+	if(!listing)
+	{
+		/* I'm not so sure about this.  FIXME We want BoardDispatch
+		* to have an initial listing maybe but perhaps we should
+		* alter BoardDispatch to be more adaptable in case there
+		* is no listing rather than create a dummy one like this
+		* and just hope it gets filled in later */
+		listing = new GameListing();
+		listing->number = key;
+		registerGameListing(listing);
+	}
+	/* Look up game information to pass to board dispatch, if any 
+	* ... actually that might be circular, let's NOT do that.
+	* Except, we don't want to overwrite an existing listing */
+
+	/*GameListing * temp_listing = new GameListing();
+	temp_listing->number = key;
+	listing = registerGameListing(temp_listing);
+	delete temp_listing;*/
+	
+	return new BoardDispatch(connection, listing);
+}
+
+std::map<PlayerListing *, unsigned short> removed_player;
+void Room::recvPlayerListing(PlayerListing * player)
+{
+	PlayerListing * p = 0;	//unused ?
+	if(!player->online)
+	{
+		removed_player[player] = player->playing;
+#ifdef FIXME
+		qDebug("Removing player %s %p on attached game %d", player->name.toLatin1().constData(), player, player->playing);
+#endif //FIXME
+		/* To prevent crashes due to GameListing link to PlayerListing
+		 * FIXME */
+		if(player->playing)
+		{
+			/* FIXME, something should clear this when games end */
+			GameListing * g = getGameListing(player->playing);
+			if(g)
+			{
+				if(g->black == player)
+				{
+					g->black = 0;
+					g->_black_name = player->name;
+					g->_black_rank = player->rank;
+				}
+				else if(g->white == player)
+				{
+					g->white = 0;
+					g->_white_name = player->name;
+					g->_white_rank = player->rank;
+				}
+			}
+		}
+		if(player->observing)
+		{
+			/* FIXME, something should clear this when games end */
+			BoardDispatch * b = connection->getIfBoardDispatch(player->observing);
+			if(b)
+			{
+#ifdef FIXME
+				qDebug("Removing player %s on observing game %d", player->name.toLatin1().constData(), player->observing);
+#endif //FIXME
+				b->recvObserver(player, false);
+			}
+			if(player->dialog_opened)
+				connection->closeTalk(*player);
+		}
+	}
+	if(playerListingIDRegistry)
+	{
+		if(player->online)
+			p = playerListingIDRegistry->getEntry(player->id, player);
+		else
+			playerListingIDRegistry->deleteEntry(player->id);	
+	}
+	else
+	{
+		if(player->online)
+			p = playerListingRegistry->getEntry(player->name, player);
+		else
+			playerListingRegistry->deleteEntry(player->name);
+	}
+	if(p && player->online)
+	{
+		std::map<PlayerListing *, unsigned short>::iterator it;
+		it = removed_player.find(p);
+		if(it != removed_player.end())
+			removed_player.erase(it);
+	}
+	updateRoomStats();	//in case we lost one or added one
 }
 
 /* These two may want to check that they are the mainwindowroom or the
@@ -364,14 +584,75 @@ void Room::recvExtPlayerListing(class PlayerListing * player)
 	mainwindow->slot_statsPlayer(player);
 }
 
-void Room::talkOpened(TalkDispatch * d)
+void Room::recvGameListing(GameListing * game)
 {
-	Talk * dlg = d->getDlg();
-	mainwindow->talkOpened(dlg);
+	unsigned int key;
+	/* FIXME This is iffy, we're using the other ID registry's
+	 * existence to indicate an ORO connection in order
+	 * to use a different key.  Ugly.  Once this works, 
+	 * we need to find a way to hide this within the
+	 * connection.  It shouldn't be running around here */
+	/* FIXME, turns out that game_number is better for listings
+	 * even if we need the code for joining, etc.  Its unique
+	 * anyway, so who cares */
+	//if(playerListingIDRegistry)
+	//	key = game->game_code;
+	//else
+		key = game->number;
+	if(game->running)
+		gameListingRegistry->getEntry(key, game);
+	else
+		gameListingRegistry->deleteEntry(key);
+	updateRoomStats();	//in case we lost one or added one
 }
 
-void Room::recvToggle(int type, bool val)
+void Room::sendStatsRequest(PlayerListing & opponent)
 {
-	mainwindow->slot_checkbox(type, val);
+	Talk * talk;
+	/* Whenever a talk window is opened, we want stats.  This
+	 * means its easier to create the talk window and let it
+	 * always create stats, then send out stats messages
+	 * that generate talk windows */
+	/* This is a little weird now...almost like we're just asking
+	 * for an update on the references */
+	talk = connection->getTalk(opponent);
+	//connection->sendStatsRequest(opponent);
+	/* This is really only for ORO, and let's see if it works but... */
+	if(talk)
+		talk->updatePlayerListing();
 }
 
+/*** GameListing/PlayerListing Registry functions ***/
+
+void GameListingRegistry::initEntry(GameListing * l)
+{
+	gamesListModel->updateListing(l);
+}
+
+void GameListingRegistry::onErase(GameListing * l)
+{
+	gamesListModel->removeListing(l);
+	delete l;
+}
+
+void PlayerListingRegistry::initEntry(PlayerListing * l)
+{
+	playerListModel->updateListing(l);
+}
+
+void PlayerListingRegistry::onErase(PlayerListing * l)
+{
+	playerListModel->removeListing(l);
+	delete l;
+}
+
+void PlayerListingIDRegistry::initEntry(PlayerListing * l)
+{
+	playerListModel->updateListing(l);
+}
+
+void PlayerListingIDRegistry::onErase(PlayerListing * l)
+{
+	playerListModel->removeListing(l);
+	delete l;
+}
