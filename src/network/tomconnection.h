3d316dd524cf4a145011c84c70d58a78066f1747
peterius <peterius@cb95479e-180f-4b2e-960d-988ef6f66277>
2009-07-18 23:56:42 +0000

    tom upgraded their server with respect to game lists

--- src/network/tomconnection.h
+++ src/network/tomconnection.h
@@ -8,6 +8,5 @@ class TomConnection : public TygemConnection
 	private:
 		virtual int requestServerInfo(void);
 		virtual void handleServerInfo(unsigned char *, unsigned int);
-		virtual void handleGamesList(unsigned char * msg, unsigned int size);
 		virtual QByteArray getTygemGameRecordQByteArray(class GameData *);
 };
