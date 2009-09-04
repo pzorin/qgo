996862f255452777b7ed4a0dac5cc115581826f1
peterius <peterius@cb95479e-180f-4b2e-960d-988ef6f66277>
2009-09-04 20:06:37 +0000

    moved file search that's probably unnecessary now anyway

--- src/audio/audio.cpp
+++ src/audio/audio.cpp
@@ -1,7 +1,9 @@
 /***************************************************************************
- *   Copyright (C) 2006 by Emmanuel Béranger   *
+ *   Copyright (C) 2009 by The qGo Project                                 *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
+ *   This file is part of qGo.   					   *
+ *                                                                         *
+ *   qGo is free software: you can redistribute it and/or modify           *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
@@ -12,8 +14,8 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ *   or write to the Free Software Foundation, Inc.,                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
@@ -30,23 +32,6 @@ QSoundSound::QSoundSound(const QString &filename, QObject *parent)
 	: Sound(filename, parent)
 {
 	qSound = new QSound(filename, this);
-
-	if (!QFile::exists(filename))		// jm 071106 start - in case file is not at the right place OR Windows OS
-	{
-		delete qSound;
-
-		QStringList sl;
-		sl << QDir::homePath() + "/ressources/sounds" 
-			<< "../ressources/sounds"
-			<< "../../src/ressources/sounds"
-			<< "../src/ressources/sounds"
-			<< ":/ressources/sounds";
-
-		QDir::setSearchPaths("sounds", sl);
-
-		QFile f("sounds:" + filename.right(filename.length() - filename.lastIndexOf('/') - 1));
-		qSound = new QSound(f.fileName());
-	}		// jm end
 }
 
 void QSoundSound::play(void)
@@ -56,9 +41,25 @@ void QSoundSound::play(void)
 
 Sound *SoundFactory::newSound(const QString &filename, QObject *parent)
 {
+	QFile * f;
+	if (!QFile::exists(filename))
+	{
+		QStringList sl;
+		sl << QDir::homePath() + "/ressources/sounds" 
+			<< "../ressources/sounds"
+			<< "../../src/ressources/sounds"
+			<< "../src/ressources/sounds"
+			<< ":/ressources/sounds";
+
+		QDir::setSearchPaths("sounds", sl);
+
+		f = new QFile("sounds:" + filename.right(filename.length() - filename.lastIndexOf('/') - 1));
+	}
+	else
+		f = new QFile(filename);
 #ifdef Q_OS_LINUX
-	return new QAlsaSound(filename, parent);
+	return new QAlsaSound(f->fileName(), parent);
 #else
-	return new QSoundSound(filename, parent);
+	return new QSoundSound(f->fileName(), parent);
 #endif
 }
