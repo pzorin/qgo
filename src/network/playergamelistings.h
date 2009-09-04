6725258f77e72c35d4b7a505781b43fea78045fa
peterius <peterius@cb95479e-180f-4b2e-960d-988ef6f66277>
2009-09-04 19:32:47 +0000

    signed to unsigned

--- src/network/playergamelistings.h
+++ src/network/playergamelistings.h
@@ -1,3 +1,25 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by The qGo Project                                 *
+ *                                                                         *
+ *   This file is part of qGo.   					   *
+ *                                                                         *
+ *   qGo is free software: you can redistribute it and/or modify           *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ *   or write to the Free Software Foundation, Inc.,                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
 #ifndef PLAYERGAMELISTING_H
 #define PLAYER_GAMELISTING_H
 
@@ -18,9 +40,9 @@ struct PlayerListing
 	unsigned int wins;
 	unsigned int losses;
 	unsigned int rated_games;
-	int observing;			//used by IGS and ORO, so not ready to get rid of it			
+	unsigned int observing;			//used by IGS and ORO, so not ready to get rid of it
 	std::vector <unsigned short> room_list; 	//used by Tygem et al.
-	int playing;
+	unsigned int playing;
 	QString extInfo;
 	QString email_address;
 
