b263d7f4d6a007a69ac90281b9e4d9dbcd878591
peterius <peterius@cb95479e-180f-4b2e-960d-988ef6f66277>
2009-09-04 19:45:41 +0000

    Position and MoveNum structs only used by sgfparser

--- src/sgf/sgfparser.cpp
+++ src/sgf/sgfparser.cpp
@@ -1,3 +1,25 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by The qGo Project                                 *
+ *                                                                         *
+ *   This file is part of qGo.   					   *
+ *                                                                         *
+ *   qGo is free software: you can redistribute it and/or modify           *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ *   or write to the Free Software Foundation, Inc.,                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
 /*
 * sgfparser.cpp
 * This class deals with SGF input. It puts data into a text stream (for cilpboard exchange)
@@ -19,9 +41,11 @@
 #endif
 
 
-
 #define STR_OFFSET 2000
 /* #define DEBUG_CODEC */
+
+struct Position { int x, y; };
+struct MoveNum {int n; };
 
 /* FIXME consider a parser for other file types, what few there are.
  * but for instance the tygem protocols use .gibo format and that code
