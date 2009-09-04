854501b170a31c6ca0cdaf15bb20e850e8b7d0c3
peterius <peterius@cb95479e-180f-4b2e-960d-988ef6f66277>
2009-09-04 19:42:05 +0000

    removed commented out

--- src/gamedata.h
+++ src/gamedata.h
@@ -1,9 +1,27 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by The qGo Project                                 *
+ *                                                                         *
+ *   This file is part of qGo.   					   *
+ *                                                                         *
+ *   qGo is free software: you can redistribute it and/or modify           *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ *   or write to the Free Software Foundation, Inc.,                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
 #ifndef GAMEDATA_H
 #define GAMEDATA_H
-
-/*
-* Global game data structure
-*/
 
 class GameResult;
 
@@ -21,9 +39,6 @@ class GameData
 			 handicap(0),
 			 komi(5.5),
 			 timeSystem(canadian),
-			 //byoTime(0),
-			 //byoPeriods(0),
-			 //byoStones(25),
 			 timelimit(0),
 			 maintime(0),
 			 periodtime(0),
@@ -71,9 +86,6 @@ class GameData
 				board_size = d->board_size;
 				handicap = d->handicap;
 				number = d->number;
-				//byoTime = d->byoTime;
-				//byoPeriods = d->byoPeriods;
-				//byoStones = d->byoStones;
 				timelimit = d->timelimit;
 				style = d->style;
 				komi = d->komi;
@@ -128,7 +140,6 @@ class GameData
 		QString By;			//these two need to be changed somehow
 		QString FR;
 		GameMode gameMode;
-		//struct BoardRecord * board;
 		
 		unsigned short game_code;
 		unsigned short opponent_id;
