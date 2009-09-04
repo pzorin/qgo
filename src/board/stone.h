94c860b631c8a96d753d8bc374419bd7ba19ab4d
peterius <peterius@cb95479e-180f-4b2e-960d-988ef6f66277>
2009-09-04 19:59:33 +0000

    changed defines.h dependency to graphicsitemstypes

--- src/board/stone.h
+++ src/board/stone.h
@@ -1,11 +1,29 @@
-/*
-* stone.h
-*/
+/***************************************************************************
+ *   Copyright (C) 2009 by The qGo Project                                 *
+ *                                                                         *
+ *   This file is part of qGo.   					   *
+ *                                                                         *
+ *   qGo is free software: you can redistribute it and/or modify           *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ *   or write to the Free Software Foundation, Inc.,                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 
 #ifndef STONE_H
 #define STONE_H
 
-#include "../defines.h"
+#include "graphicsitemstypes.h"
 
 #include <QtGui>
 
