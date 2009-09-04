9b0b02b0dff7f748062f3588d4e155c9f652b254
peterius <peterius@cb95479e-180f-4b2e-960d-988ef6f66277>
2009-09-04 06:16:20 +0000

    unused

--- src/network/connectiontypes.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONNECTIONTYPES_H
-#define CONNECTIONTYPES_H
-/* This should be moved into defines.h later as well as reconciled with
- * existing server name types */
-
-#endif //CONNECTIONTYPES_H
