3f09f585fe40d31bc9c3481660b5d5c939e23635
peterius <peterius@cb95479e-180f-4b2e-960d-988ef6f66277>
2009-09-23 13:31:46 +0000

    fucked up the test again

--- src/gtp/qgtp.cpp
+++ src/gtp/qgtp.cpp
@@ -185,7 +185,7 @@ void QGtp::slot_readFromStdout()
 		qDebug("0");
 
 	buff = _response[0];
-	qDebug("*%s*", _response.toLatin1().constData()[0]);
+	qDebug("*%s*", _response.toLatin1().constData());
 	if(buff != "=" && buff != "?")
 	{
 		responseReceived = FALSE;
