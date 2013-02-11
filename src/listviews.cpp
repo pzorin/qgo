caf548079c6b5245d7b29eac56cb92b8d2059420
Pavel Zorin-Kranich <pazo@fa.uni-tuebingen.de>
2013-02-11 11:55:57 +0100

    Fix compilation error with gcc 4.7

--- src/listviews.cpp
+++ src/listviews.cpp
@@ -342,7 +342,7 @@ void GamesListModel::removeListing(GameListing * const l)
 {
 	for(int i = 0; i < items.count(); i++)
 	{
-		GamesListItem * const item = static_cast<const GamesListItem *>(items[i]);
+		const GamesListItem * item = static_cast<const GamesListItem *>(items[i]);
 		if(item->getListing() == l)
 		{
 			/* Really this is supposed to be not QModelIndex() but the
