afeaab0ae643798fbcfba585160d8d9050c0c065
Pavel Zorin-Kranich <pazo@fa.uni-tuebingen.de>
2014-05-11 20:29:29 +0300

    Use a spinbox for komi input

--- src/newgamedialog.cpp
+++ src/newgamedialog.cpp
@@ -21,7 +21,7 @@ NewGameDialog::NewGameDialog(MainWindow *parent) :
     ui->newFile_Handicap->setValue(settings.value("EDIT_HANDICAP").toInt());
     if((var = settings.value("EDIT_KOMI")) == QVariant())
         var = 5.5;
-    ui->newFile_Komi->setText(var.toString());
+    ui->newFile_Komi->setValue(var.toDouble());
 
     ui->computerPlaysWhite->setChecked(settings.value("COMPUTER_PLAYS_WHITE").toBool());
     ui->computerPlaysBlack->setChecked(settings.value("COMPUTER_PLAYS_BLACK").toBool());
@@ -35,7 +35,7 @@ NewGameDialog::~NewGameDialog()
     //SGF edition tab default values
     settings.setValue("EDIT_SIZE",ui->newFile_Size->value());
     settings.setValue("EDIT_HANDICAP",ui->newFile_Handicap->value());
-    settings.setValue("EDIT_KOMI",ui->newFile_Komi->text());
+    settings.setValue("EDIT_KOMI",ui->newFile_Komi->value());
 
     settings.setValue("COMPUTER_PLAYS_WHITE", ui->computerPlaysWhite->isChecked());
     settings.setValue("COMPUTER_PLAYS_BLACK", ui->computerPlaysBlack->isChecked());
@@ -57,7 +57,7 @@ void NewGameDialog::accept()
     gd->handicap = ui->newFile_Handicap->value();
     gd->black_name = ui->newFile_BlackPlayer->text();
     gd->white_name = ui->newFile_WhitePlayer->text();
-    gd->komi = ui->newFile_Komi->text().toFloat();
+    gd->komi = ui->newFile_Komi->value();
 
     BoardWindow * bw = new BoardWindow(gd, !(ui->computerPlaysBlack->isChecked()),
                                        !(ui->computerPlaysWhite->isChecked()));
